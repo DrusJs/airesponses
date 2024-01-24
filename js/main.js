93758420834a5d1982d8a5bf8ccdae39e8c9fdb9
drusjs <magnojoky962@gmail.com>
2024-01-24 16:23:53 +0300

    add pages

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 const burger = document.getElementById('burger-button')
 const burgerContainer = document.getElementById('burger-menu')
+
 if (burger) {
     burger.addEventListener('click', function(event) {
         burger.classList.toggle('active')
-        burgerContainer.parentElement.classList.toggle('active')
+        burgerContainer.closest('.burger-blur').classList.toggle('active')
     })
-    burgerContainer.parentElement.addEventListener('click', function(event) {
+    burgerContainer.closest('.burger-blur').addEventListener('click', function(event) {
         if (event.target.classList.contains('burger-blur')) {
             burger.classList.remove('active')
-            burgerContainer.parentElement.classList.remove('active')
+            burgerContainer.closest('.burger-blur').classList.remove('active')
         }        
+    })
+}
+
+const headerSelect = document.querySelectorAll('.header-select-head')
+if (headerSelect.length) {
+    headerSelect.forEach(el=>{
+        el.addEventListener('click', function(event) {
+            event.currentTarget.parentElement.classList.toggle('active')
+        })
     })
 }
